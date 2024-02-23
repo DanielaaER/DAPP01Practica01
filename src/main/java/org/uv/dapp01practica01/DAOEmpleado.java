fafb291c7c1220faf26f15ab9a328fd6e57e3f90
Daniela Espinoza <danes@roandai.com>
2024-02-23 10:01:55 -0600

    add search all

--- src/main/java/org/uv/dapp01practica01/DAOEmpleado.java
+++ src/main/java/org/uv/dapp01practica01/DAOEmpleado.java
@@ -11,13 +11,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DAOEmpleado implements IDAO<PojoEmpleado, Integer> {
-    
+
     private final Conexion databaseConnection;
-    
+
     public DAOEmpleado() {
         this.databaseConnection = Conexion.getInstance();
     }
-    
+
     @Override
     public boolean guardar(PojoEmpleado empleado) {
         Conexion con = Conexion.getInstance();
@@ -36,12 +36,12 @@ public class DAOEmpleado implements IDAO<PojoEmpleado, Integer> {
                     Logger.getLogger(DAOEmpleado.class.getName()).log(Level.SEVERE, null, ex);
                     return false;
                 }
-                
+
             }
         };
         return con.execute(tra);
     }
-    
+
     @Override
     public boolean eliminar(Integer id) {
         Conexion con = Conexion.getInstance();
@@ -62,7 +62,7 @@ public class DAOEmpleado implements IDAO<PojoEmpleado, Integer> {
         };
         return con.execute(tra);
     }
-    
+
     @Override
     public boolean modificar(PojoEmpleado empleado, Integer id) {
         Conexion con = databaseConnection;
@@ -86,7 +86,7 @@ public class DAOEmpleado implements IDAO<PojoEmpleado, Integer> {
         };
         return con.execute(tra);
     }
-    
+
     @Override
     public PojoEmpleado buscarById(Integer id) {
         Conexion con = databaseConnection;
@@ -113,42 +113,43 @@ public class DAOEmpleado implements IDAO<PojoEmpleado, Integer> {
                     return null;
                 }
             }
-            
+
         };
         PojoEmpleado empleado = (PojoEmpleado) con.select(select).get(0);
-        
         return empleado;
-        
+
     }
-    
+
     @Override
     public List<PojoEmpleado> buscarAll() {
-        List<PojoEmpleado> empleados = new ArrayList<>();
-        TransaccionDB tra = new TransaccionDB<List<PojoEmpleado>>(empleados) {
+
+        Conexion con = databaseConnection;
+        SelectionDB select = new SelectionDB() {
             @Override
-            public boolean execute(Connection con) {
+            public List buscar(Connection con) {
                 try {
                     String sql = "SELECT * FROM empleadotemporal";
-                    Statement st = con.createStatement();
-                    ResultSet result = st.executeQuery(sql);
+                    PreparedStatement pstm = con.prepareStatement(sql);
+                    ResultSet result = pstm.executeQuery();
+                    List lst = new ArrayList<PojoEmpleado>();
+                    PojoEmpleado p = new PojoEmpleado();
                     while (result.next()) {
-                        PojoEmpleado empleado = new PojoEmpleado();
-                        empleado.setId(result.getInt("id"));
-                        empleado.setNombre(result.getString("nombre"));
-                        empleado.setDireccion(result.getString("direccion"));
-                        empleado.setTelefono(result.getString("telefono"));
-                        empleados.add(empleado);
+                        p.setId(result.getInt("id"));
+                        p.setNombre(result.getString("nombre"));
+                        p.setDireccion(result.getString("direccion"));
+                        p.setTelefono(result.getString("telefono"));
+                        lst.add(p);
                     }
-                    return true;
-                    
+                    return lst;
                 } catch (SQLException ex) {
-                    Logger.getLogger(DAOEmpleado.class
-                            .getName()).log(Level.SEVERE, null, ex);
-                    return false;
+                    Logger.getLogger(DAOEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+                    return null;
                 }
             }
+
         };
-        databaseConnection.execute(tra);
+
+        List<PojoEmpleado> empleados = (List<PojoEmpleado>) (PojoEmpleado) con.select(select);
         return empleados;
     }
 }
